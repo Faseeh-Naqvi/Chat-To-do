defad69d2982d39045cfd065ce6ceeabe08470b3
zain <zsyed44@uwo.ca>
2025-01-16 01:34:10 -0500

    update to Ranker.py

--- Mathematical-Approach/Ranker.py
+++ Mathematical-Approach/Ranker.py
@@ -6,9 +6,16 @@
 # Faseeh Naqvi & Zain Syed
 # January 15th 2025
 
-taskArray = [{"Task": "Create Chat-To-Do", "Completion": 50, "Deadline": 10, "Difficulty": 20, "Interest": 6},]
+taskArray = [
+        {"Task": "Create Chat-To-Do", "Completion": 50, "Deadline": 10, "Difficulty": 20, "Interest": 6},
+        {"Task": "Finish Report", "Completion": 30, "Deadline": 5, "Difficulty": 15, "Interest": 8},
+        {"Task": "Prepare Presentation", "Completion": 80, "Deadline": 2, "Difficulty": 10, "Interest": 9}
+    ]
 # Completion (in hours), Deadline (in days), Difficulty (1-100), Interest (1-10)
+
 allTasksArray = []
+criticalPath = []
+late = []
 
 for task in taskArray:
     score = 0
@@ -17,10 +24,36 @@ for task in taskArray:
     deadline = task["Deadline"]
     difficulty = task["Difficulty"]
     interest = task["Interest"]
-
-    if completion < deadline:
-        score += 150
     
-    score += (1/deadline) * 100
+
+def criticalPathAnalysis(): #The purpose of this function is to determine what order the tasks should be completed in SOLELY based on how long they take, and their deadlines.
+    criticalPath = sorted(taskArray, key=lambda task: task['Deadline'])
+
+    for task in criticalPath:
+        if (task["Deadline"] < task["Completion"]*24):
+            criticalPath.remove(task)
+            late.append(task)
+            print("Task: " + task["Task"] + " is late")
+    
+    for task in late:
+        late = sorted(late, key=lambda task: (task['Completion'] - task['Deadline']*24))
+    
+    for task in criticalPath:
+        print(task["Task"])
+    for task in late:
+        print(task["Task"])
+
+criticalPathAnalysis()
 
 
+
+#
+# 1) D=20 C=100
+# 2) D=10 C=50
+# 3) D=5 C=30
+# 
+# 5) D=1 C=10
+#
+#
+#
+# 4) D=2 C=80
